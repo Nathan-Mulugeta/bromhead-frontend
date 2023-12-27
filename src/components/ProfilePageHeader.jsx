43ca8f2a257754f521b516461adbe4eb70ad9cb1
nathan <nathan>
2023-12-27 09:50:29 +0300

    fix avatar bg color

--- src/components/ProfilePageHeader.jsx
+++ src/components/ProfilePageHeader.jsx
@@ -3,30 +3,10 @@ import useAuth from "../hooks/useAuth";
 import { useGetUsersQuery } from "../slices/users/usersApiSlice";
 import { Avatar, Chip, Typography } from "@mui/material";
 
-function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = "#";
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
 function stringAvatar(name) {
   return {
     sx: {
-      bgcolor: stringToColor(name),
+      bgcolor: "background.light",
       fontSize: 80,
       width: 150,
       height: 150,
