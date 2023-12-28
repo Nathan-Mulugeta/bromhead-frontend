ef04e1e13455107869d9af4aa33ea8919a37b6d5
nathan <nathan>
2023-12-29 00:34:34 +0300

    fix animate ping bug

--- src/components/DataDisplayItem.jsx
+++ src/components/DataDisplayItem.jsx
@@ -53,7 +53,9 @@ const DataDisplayItem = ({ label, value, icon }) => {
               <>
                 <span class="relative flex h-3 w-3">
                   <span
-                    class={`absolute inline-flex h-full w-full animate-ping rounded-full ${statusColor} opacity-75`}
+                    class={`absolute inline-flex h-full w-full ${
+                      statusColor === "bg-green-400" ? "animate-ping" : ""
+                    }  rounded-full ${statusColor} opacity-75`}
                   ></span>
                   <span
                     class={`relative inline-flex h-3 w-3 rounded-full ${statusColor}`}
