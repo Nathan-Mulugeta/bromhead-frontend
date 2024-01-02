2c83830072a9ef8186ed775728c25eb93cbc8fd2
nathan <nathan>
2024-01-03 01:04:35 +0300

    add the logo in the middle of the screen temporarily

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,13 @@
-import { LuBell } from "react-icons/lu";
-import { CiSearch } from "react-icons/ci";
 import NavbarProfile from "./NavbarProfile";
+import Logo from "../assets/bromhead-logo.svg";
 
 const NavBar = () => {
   return (
     <div className="sticky left-0 right-0 top-0 z-20 flex bg-backgroundLight">
       <nav className="container mx-auto flex items-center justify-between p-1 pl-9 sm:pl-3">
-        <div className="flex items-center gap-4">
-          {/* <div>
-            <CiSearch color="#101317" fontSize={20} />
-          </div> */}
-        </div>
-        <div className="flex items-center gap-4">
-          {/* <div>
-            <LuBell color="#101317" fontSize={20} />
-          </div> */}
+        <div className="w-10 sm:opacity-0"></div>
+        <img className="h-14 w-14 sm:opacity-0" src={Logo} />
+        <div className="flex items-center justify-between gap-4">
           <div>
             <NavbarProfile />
           </div>
