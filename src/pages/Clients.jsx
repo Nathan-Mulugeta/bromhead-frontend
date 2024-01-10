779e753dd972af4fe06c3710c65a7515652469fc
nathan <nathan>
2024-01-10 17:02:56 +0300

    replace clients list with data grid

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -10,6 +10,16 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { setLoading } from "../slices/loading/loadingSlice";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "name", headerName: "Client Name", minWidth: 140 },
+  {
+    field: "address",
+    headerName: "Address",
+    width: 150,
+  },
+];
 
 const Clients = () => {
   useTitle("Clients");
@@ -40,60 +50,23 @@ const Clients = () => {
     dispatch(setLoading(isLoading));
   }, [dispatch, isLoading]);
 
+  const rows = [];
+
   if (isSuccess) {
     const { ids, entities } = clients;
 
-    if (ids?.length === 0) return (content = <p>No content</p>);
-
-    content =
-      ids.length &&
-      ids.map((clientId) => {
-        const client = entities[clientId];
-
-        return (
-          <List key={clientId}>
-            <ListItem disablePadding>
-              <ListItemButton onClick={() => handleClick(clientId)}>
-                <ListItemText
-                  primary={client.name}
-                  secondary={
-                    <>
-                      <Typography
-                        sx={{ display: "block" }}
-                        component="span"
-                        variant="body1"
-                        color="text.primary"
-                      >
-                        {client.contactInfo.address}
-                      </Typography>
-                      {client.contactInfo.email && (
-                        <Typography
-                          sx={{ display: "block" }}
-                          component="span"
-                          variant="body2"
-                          color="text.secondary"
-                        >
-                          Email: {client.contactInfo.email}
-                        </Typography>
-                      )}
-                      <Typography
-                        sx={{ display: "block" }}
-                        component="span"
-                        variant="body2"
-                        color="text.secondary"
-                      >
-                        {client.contactInfo.contactPersonPosition} Phone:{" "}
-                        {client.contactInfo.phone}
-                      </Typography>
-                    </>
-                  }
-                />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        );
+    ids.map((id) => {
+      rows.push({
+        id,
+        name: entities[id].name,
+        address: entities[id].contactInfo.address,
       });
+    });
   }
+
+  const handleRowClick = (params) => {
+    navigate(`/dash/clients/${params.id}`);
+  };
 
   return (
     <div>
@@ -120,7 +93,26 @@ const Clients = () => {
           color: "primary.contrastText",
         }}
       >
-        {content}
+        <DataGrid
+          sx={{
+            border: "none",
+            p: 1,
+          }}
+          onRowClick={handleRowClick}
+          columns={columns}
+          rows={rows}
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+            },
+          }}
+          initialState={{
+            pagination: { paginationModel: { pageSize: 10 } },
+          }}
+          pageSizeOptions={[5, 10, 25]}
+          disableRowSelectionOnClick
+        />
       </Box>
     </div>
   );
